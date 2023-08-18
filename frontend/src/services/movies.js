28b0023cb680e6c23955e5ec2c23b0429dc92a91
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-08-18 11:38:25 +0300

    configure the backend url

--- frontend/src/services/movies.js
+++ frontend/src/services/movies.js
@@ -1,33 +1,36 @@
 import axios from "axios";
 
+const backend_url = "https://movie-review-app-backend-n0ij.onrender.com/api/v1/movies";
+const localhost_url = "http://localhost:5001/api/v1/movies"
+
 class MovieDataService{
 
     getAll(page=0){
-        return axios.get(`http://localhost:5001/api/v1/movies?page=${page}`);
+        return axios.get(`${backend_url}?page=${page}`);
     };
 
     get(id){
-        return axios.get(`http://localhost:5001/api/v1/movies/id/${id}`);
+        return axios.get(`${backend_url}/id/${id}`);
     };
 
     find(query, by="title", page = 0){
-        return axios.get(`http://localhost:5001/api/v1/movies?${by}=${query}&page=${page}`);
+        return axios.get(`${backend_url}?${by}=${query}&page=${page}`);
     };
 
     createReview(data){
-        return axios.post(`http://localhost:5001/api/v1/movies/review`, data);
+        return axios.post(`${backend_url}/review`, data);
     };
 
     updateReview(data){
-        return axios.put(`http://localhost:5001/api/v1/movies/review`, data);
+        return axios.put(`${backend_url}/review`, data);
     };
 
     deleteReview(id, userId){
-        return axios.delete("http://localhost:5001/api/v1/movies/review", {data: {review_id: id, user_id: userId}});
+        return axios.delete(`${backend_url}/review`, {data: {review_id: id, user_id: userId}});
     };
 
     getRatings(){
-        return axios.get(`http://localhost:5001/api/v1/movies/ratings`);
+        return axios.get(`${backend_url}/ratings`);
     };
 };
 
